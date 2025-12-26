423793fedaf93c511d014112db2b950e302474b5
Clevison <cleverson-altF4>
2025-12-26 18:55:55 -0500

    mudança de bug

--- Conversor Fahrenheit/conversor.c
+++ Conversor Fahrenheit/conversor.c
@@ -4,53 +4,53 @@
 3. Sair*/
 //Fórmula: F = (C * 9/5) + 32
 
+
+        
 #include <stdio.h>
 
-int main(){
-    int opcao;
-    float celsius, fahr;
-    do
-    {
-        printf("\n###### Conversor  Fahrenheit ######\n ");
+int main() {
+    int opcao = 0;
+    float celsius = 0.0, fahr = 0.0;
+    
+    do {
+        printf("\n###### Conversor de Temperatura ######\n");
         printf(" 1 - Celsius para Fahrenheit\n");
         printf(" 2 - Fahrenheit para Celsius\n");
         printf(" 3 - Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
-
-        switch (opcao)
-        {
-        case 1:
-            printf("Digite um número em Celsius: ");
-            scanf("%f", &celsius);
-            fahr = (celsius * 9.0/5.0)+32.0;
-            printf("celsius, °C = %.2f °F", fahr);
-            break;
-
-        case 2:
-            printf("Digite um valor em Fahrenheit");
-            scanf("%f", &fahr);
-            celsius = (fahr - 32.0) * 5.0/9.0;
-            printf("Para fahreinheit °F = %.2f\n", celsius);
-            break;
         
-        case 3:
-            printf("Digite 0 p/ sair");
-            break;
-        default:
-            printf("burro!");
-            break;
+        // Limpar o buffer do teclado
+        while (getchar() != '\n');
+
+        switch (opcao) {
+            case 1:
+                printf("Digite a temperatura em Celsius: ");
+                scanf("%f", &celsius);
+                fahr = (celsius * 9.0/5.0) + 32.0;
+                printf("%.2f °C = %.2f °F\n", celsius, fahr);
+                break;
+                
+            case 2:
+                printf("Digite a temperatura em Fahrenheit: ");
+                scanf("%f", &fahr);
+                celsius = (fahr - 32.0) * 5.0/9.0;
+                printf("%.2f °F = %.2f °C\n", fahr, celsius);
+                break;
+                
+            case 3:
+                printf("Saindo...\n");
+                break;
+                
+            default:
+                printf("Opção inválida! Digite 1, 2 ou 3.\n");
+                break;
         }
-
-
-    } while (opcao != 0);
-
-    if (opcao != 3)
-    {
-        getchar();
-    }
-    
+        
+       
+        while (getchar() != '\n');
+        
+    } while (opcao != 3);
 
     return 0;
-        
 }
